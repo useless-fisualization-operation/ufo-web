56d0ac5fb3b35d1ae95eef078f7d760b361b7105
druskus20 <druskus20@gmail.com>
2023-03-10 14:56:55 +0100

    Updates

--- internal/immutable/chunks/1-7a3ac7b3.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-fc3e1d95.js";export{t as component};
---
+++ internal/immutable/chunks/2-fcf69811.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-b4a4ed4e.js";export{t as component};
