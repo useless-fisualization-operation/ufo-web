b3f78bf94b642d8a5cfca5e4878a58557ff20cd2
Annika Öhri <oehria@ethz.ch>
2023-03-18 16:15:23 +0100

    temporal filter, changing legend, warning for invalid years

--- my-app/src/components/Heatmap.svelte
+++ my-app/src/components/Heatmap.svelte
@@ -3,10 +3,13 @@
 
 <div class="heatmap">
 	<h4 class="title">UFO sightings per day of the year</h4>
-
+	Hover over a day or use filters to find out more!
 	<div id="my_dataviz" />
+	year: <input type="string" id="year_txt"/> ("all" for all)
+	<br>
 	<select class="stateDrop" name="stateDrop"></select>
 
+	
 	<script>
 		function sleep(time) {
 			return new Promise((resolve) => {
@@ -33,7 +36,6 @@
 
 			// Labels of row and columns
 			var myGroups = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
-			var months = ['Jan', 'Feb', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
 			var myVars = [
 				'1',
 				'2',
@@ -96,6 +98,7 @@
 				'November',
 				'December'
 			];
+			let days_per_month=[31,29,31,30,31,30,31,31,30,31,30,31]
 			let state_indices =
 				//changed for full rel
 				[
@@ -271,8 +274,18 @@
 				day=d.day+"rd"
 				}
 				var month=fullmonths[d.month-1]
+				var year=d3.select('#year_txt').property('value')
+				var text="On the "+day+" of "+month+" there were <br>" + d.frequency+" UFO sightings in year "+year
+				if(year=="all"){
+					text="On the "+day+" of "+month+" there were <br>" + d.frequency+" UFO sightings in total"
+				}
+				if(year!="all"){
+					if(isNaN(parseInt(year)) || parseInt(year)<1639 || parseInt(year)>2023){
+						text="Please enter a year between 1639 and 2023 or \"all\" to see the UFO sighting frequency displayed"
+					}
+				}
 				tooltip
-				.html("On the "+day+" of "+month+" there were <br>" + d.frequency+" UFO sightings in total")
+				.html(text)
 				.style("left", (d3.mouse(this)[0]+70) + "px")
 				.style("top", (d3.mouse(this)[1]) + "px")
 			}
@@ -296,6 +309,12 @@
     d3.select('select.stateDrop')
     .property('value','all')
 
+	d3.select('#year_txt').on('change', () => {
+					update(data);
+				});
+
+	d3.select('#year_txt').property('value', "all");
+
     myColor = d3.scaleLinear()
       .range(["white", "#ff6f08"])
       .domain([0,2082])
@@ -306,10 +325,17 @@
       .labelFormat(d3.format(".0f"))
       .title("Number of UFO Sightings");
 
+	//legend.labels(["hi", "70"])
+
     svg.append("g")
     .attr("class","legendClass")
       .attr("transform", "translate(500,100)")
       .call(legend);
+	
+	legend.labels(["hi", "70"])
+
+	svg.select("g").select("legendClass").remove()
+
 
     update(data)
   })
@@ -317,6 +343,7 @@
   function update(data){
     //data preprocessing (get frequency)
     var state=d3.select('select.stateDrop').property('value')
+	var year=d3.select('#year_txt').property('value')
     var tally = {};
     var last_Date='';
     data.forEach(function(line) {
@@ -325,27 +352,49 @@
         var date=parts[0];
         var month=date.split('/')[0]
         var day=date.split('/')[1]
-        date=parseInt(day)+'/'+parseInt(month)//ignore year for now
-        //console.log(date);
-        tally[date] = (tally[date]||0) + 1;
-        last_Date=date;
+		var yr=date.split('/')[2]
+		if(year=='all' || parseInt(yr)==parseInt(year)){
+			date=parseInt(day)+'/'+parseInt(month)
+			tally[date] = (tally[date]||0) + 1;
+			last_Date=date;
+		}
       }
     });
 
     var data_ref = [];
-    for (var date in tally) {
-        if (tally.hasOwnProperty(date)) {
-          if(!isNaN(date.split('/')[1])&&!isNaN(date.split('/')[1])){
-            data_ref.push({
-                month: date.split('/')[1],
-                day: date.split('/')[0],
-                frequency: tally[date]
-            });
-          }
-        }
+	//reset to 0 first so no undefined and no old values that mean svg wont be overwritten
+	for(let monthh=1; monthh<13; monthh++){
+		for(let dayy = 1; dayy < days_per_month[monthh-1]+1; dayy++) { 
+			var fr=0
+			console.log(dayy+'/'+monthh)
+			if(!isNaN(parseInt(tally[dayy+'/'+monthh]))){//this date is in data
+			fr=tally[dayy+'/'+monthh]
+			}
+			data_ref.push({
+					month: monthh,
+					day: dayy,
+					frequency: fr
+			});
+      }
     }
 
+	myColor = d3.scaleLinear()
+      .range(["white", "#995e9f"])
+      .domain([0,d3.max(data_ref, d => d.frequency)])
 
+	svg.selectAll("rect").remove()
+
+    svg.select("g.legendClass").remove()
+
+    legend = d3.legendColor()
+      .scale(myColor)
+      .labelFormat(d3.format(".0f"))
+      .title("Number of UFO Sightings");
+
+    svg.append("g")
+    .attr("class","legendClass")
+      .attr("transform", "translate(500,100)")
+      .call(legend);
   
     // add the squares
     svg.selectAll()
