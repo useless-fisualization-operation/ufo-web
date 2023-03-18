b1e1a4e4b736cedaea09b35cc1e0d568810e6c14
vfrawa <vfrawa@student.ethz.ch>
2023-03-18 16:46:38 +0100

    fixed bug for year text field constraints. Added better labels on axes for very small and very large numbers.

--- my-app/src/components/Chart.svelte
+++ my-app/src/components/Chart.svelte
@@ -17,7 +17,7 @@
 			font-size: 0.8em;
 		}
 	</style>
-	<h4 class="title">Relationship between UFO Sightings and Other Attributes</h4>
+	<h4 class="title">Relationship with Other Attributes on State Level </h4>
 
 	<div id="controls">
 		<p>
@@ -41,16 +41,6 @@
 
 	<svg id="scatter-container" />
 
-	<!--
-    <div id="slider-container"></div>
-    <div class="main">
-      <h2>Age
-        <span id="slider3textmin">0</span> - <span id="slider3textmax">0</span>
-      </h2>
-  
-      <div id="slider3" class="d3-slider d3-slider-horizontal"></div>
-    -->
-
 	<script>
 		function sleep(time) {
 			return new Promise((resolve) => {
@@ -64,7 +54,6 @@
 			// how long transitions last (msec)
 			let transitionTime = 2000;
 			let state_indices =
-				//changed for full rel
 				[
 					'AK',
 					'AL',
@@ -175,21 +164,21 @@
 
 			let longVars = {
 				Num_Sightings: 'Number of UFO sightings',
-				Rel_Num_Sightings: 'Number of UFO sightings per resident', //changed for full rel
-				Rel_Very_important: 'Religion: very important',
-				Rel_Somewhat_important: 'Religion: somewhat important',
-				Rel_Not_too_important: 'Religion: not too important',
-				Rel_Not_at_all_important: 'Religion: not at all important',
-				Rel_Dont_know: "Religion: don't know how important",
-				Population_Density: 'Population density',
+				Rel_Num_Sightings: 'Number of UFO sightings per resident',
+				Rel_Very_important: 'Religion: very important (in %)',
+				Rel_Somewhat_important: 'Religion: somewhat important (in %)',
+				Rel_Not_too_important: 'Religion: not too important (in %)',
+				Rel_Not_at_all_important: 'Religion: not at all important (in %)',
+				Rel_Dont_know: "Religion: don't know how important (in %)",
+				Population_Density: 'Population density (population per square mile)',
 				Total_Resident_Population: 'Total population',
-				Illicit_Drug_Use: 'Illicit Drug Use', //changed for full rel
-				Marijuana_Use: 'Marijuana Use', //changed for full rel
-				Illicit_Drug_Use_Other_Than_Marijuana: 'Illicit Drug Use Other Than Marijuana', //changed for full rel
-				Binge_Alcohol_Use: 'Binge Alcohol Use', //changed for full rel
-				Illicit_Drug_Use_Disorder: 'Illicit Drug Use Disorder', //changed for full rel
-				Serious_Mental_Illness: 'Serious Mental Illness', //changed for full rel
-				Any_Mental_Illness: 'Any Mental Illness' //changed for full rel
+				Illicit_Drug_Use: 'Illicit Drug Use (in %)',
+				Marijuana_Use: 'Marijuana Use (in %)',
+				Illicit_Drug_Use_Other_Than_Marijuana: 'Illicit Drug Use Other Than Marijuana (in %)',
+				Binge_Alcohol_Use: 'Binge Alcohol Use (in %)',
+				Illicit_Drug_Use_Disorder: 'Illicit Drug Use Disorder (in %)', 
+				Serious_Mental_Illness: 'Serious Mental Illness (in %)', 
+				Any_Mental_Illness: 'Any Mental Illness (in %)'
 			};
 
 			// use Margin Convention to layout the SVG with an inner plotting region
@@ -215,14 +204,10 @@
 			// load data
 			let url_other =
 				'https://raw.githubusercontent.com/vfrawa/vfrawa.github.io/main/scatterplot_data_full_relative_drugsincl.csv';
-			//let url_other = "https://raw.githubusercontent.com/vfrawa/vfrawa.github.io/main/scatterplot_data_full_relative.csv"
-			//let url_other = "https://raw.githubusercontent.com/useless-fisualization-operation/ufo-web/main/dataset/scatterplot_data.csv"
-			//let url_sight = "https://raw.githubusercontent.com/useless-fisualization-operation/ufo-web/main/dataset/num_sightings_per_state_per_year.csv"
-			//let url_sight = "https://raw.githubusercontent.com/useless-fisualization-operation/ufo-web/main/dataset/num_sightings_per_state_per_year.csv"
 			let url_sight =
 				'https://raw.githubusercontent.com/vfrawa/vfrawa.github.io/main/relative_num_sightings_per_state_per_year_timespan_rows_full.csv';
 			let url_sight_abs =
-				'https://raw.githubusercontent.com/vfrawa/vfrawa.github.io/main/num_sightings_per_state_per_year_timespan_rows_full.csv'; //changed for x axis
+				'https://raw.githubusercontent.com/vfrawa/vfrawa.github.io/main/num_sightings_per_state_per_year_timespan_rows_full.csv';
 			d3.csv(url_other, cleanup_data, function (error, d_other) {
 				if (error) {
 					console.log(error);
@@ -244,7 +229,7 @@
 				});
 			});
 
-			// d3.csv() reads in everything as a string.  convert all string numbers to numbers.
+			//convert all strings that represent numbers to numbers.
 			function cleanup_data(d) {
 				for (k of Object.keys(d)) {
 					if (k != 'State') d[k] = +d[k];
@@ -255,14 +240,13 @@
 			// declare some variables that will be assigned in functions below
 			let d_other_global = [];
 			let d_sight_global = [];
-			let d_sight_abs_global = []; //changed for x axis
+			let d_sight_abs_global = [];
 			let xScale, yScale;
 			let xAxis, yAxis;
 			let min_year = 1910,
 				max_year = 2023;
 
 			function get_state_year_idx(state, start_year, end_year) {
-				//changed for full rel
 				state_idx = state_indices.indexOf(state);
 				start_year_idx = start_year - min_year;
 				end_year_idx = end_year - min_year;
@@ -274,24 +258,35 @@
 				);
 			}
 
+			const f = formatValue = d3.format(".3s");
+
+			function axis_label_format(d) {
+			if (d==0) {
+				return 0;
+			}
+			if (d >= 1000000) {
+				return d/1000000 + " M.";
+			}
+			if (d<=0.001) {
+				return f(d)
+			}
+			return d;
+			}
+
 			// setup() is for setting up plot elements once at the beginning
 			// update() will be used for the initial draw and for redrawing upon change
-			//function setup(d_other, d_sight) {
 			function setup(d_other, d_sight, d_sight_abs) {
-				//changed for x axis
 				d_other_global = d_other;
 				d_sight_global = d_sight;
-				d_sight_abs_global = d_sight_abs; //changed for x axis
+				d_sight_abs_global = d_sight_abs;
 
 				//State is not quantitative, so it shouldn't be selected
-				//let variables = Object.keys(d_other[0]).filter(d => d != 'model')
-				//let variables = Object.keys(d_other[0]).filter(d => d != 'State' && d!="Rel_Sample_size" & d!="Num_Sightings")
 				let variables = Object.keys(d_other[0]).filter(
 					(d) =>
 						d != 'State' &&
 						(d != 'Rel_Sample_size') & (d != 'Num_Sightings') & (d != 'Rel_Num_Sightings')
-				); //changed for full rel
-				let x_variables = ['Num_Sightings', 'Rel_Num_Sightings']; //changed for x axis
+				);
+				let x_variables = ['Num_Sightings', 'Rel_Num_Sightings'];
 
 				// border around plotting region
 				/*scatterInner
@@ -299,12 +294,10 @@
 			.attr('width', innerWidth)
 			.attr('height', innerHeight)
 			.attr('fill', 'transparent')
-			//.attr('stroke', 'black')*/
 
 				// populate selectors
-				d3.select('select.xvar') //changed for x axis
-					//.on('change', () => update(d_other_global, d_sight_global)) // make sure .on() is above .selectAll()
-					.on('change', () => update(d_other_global, d_sight_global, d_sight_abs_global)) //changed for x axis
+				d3.select('select.xvar')
+					.on('change', () => update(d_other_global, d_sight_global, d_sight_abs_global))
 					.selectAll('option')
 					.data(x_variables)
 					.enter()
@@ -313,8 +306,7 @@
 					.text((d) => longVars[d]);
 
 				d3.select('select.yvar')
-					//.on('change', () => update(d_other_global, d_sight_global)) // make sure .on() is above .selectAll()
-					.on('change', () => update(d_other_global, d_sight_global, d_sight_abs_global)) //changed for x axis
+					.on('change', () => update(d_other_global, d_sight_global, d_sight_abs_global))
 					.selectAll('option')
 					.data(variables)
 					.enter()
@@ -323,17 +315,15 @@
 					.text((d) => longVars[d]);
 
 				d3.select('#button_start_year').on('change', () => {
-					//update(d_other_global, d_sight_global); //changed for x axis
 					update(d_other_global, d_sight_global, d_sight_abs_global);
 				});
 
 				d3.select('#button_end_year').on('change', () => {
-					//update(d_other_global, d_sight_global);//changed for x axis
 					update(d_other_global, d_sight_global, d_sight_abs_global);
 				});
 
 				// initialize values of select elements.
-				d3.select('select.xvar').property('value', 'Rel_Num_Sightings'); //changed for x axis
+				d3.select('select.xvar').property('value', 'Rel_Num_Sightings');
 				d3.select('select.yvar').property('value', 'Population_Density');
 				d3.select('#button_start_year').property('value', min_year);
 				d3.select('#button_end_year').property('value', max_year);
@@ -350,35 +340,25 @@
 				d3.select('#button_end_year').property('max', max_year);
 
 				// read current selections
-				//let xvar = 'Num_Sightings'
-				//let xvar = 'Rel_Num_Sightings' //changed for full rel
-				let xvar = d3.select('select.xvar').property('value'); //changed for x axis
+				let xvar = d3.select('select.xvar').property('value');
 				let yvar = d3.select('select.yvar').property('value');
 				let start_year = +d3.select('#button_start_year').property('value');
 				let end_year = +d3.select('#button_end_year').property('value');
 
 				//change Num_sightings in d_other s.t. they reflect the chosen year range
 				d_other.map((d) => {
-					//d_sight_curr = d_sight.filter(d_s => d_s["Year"] == start_year && d_s["State"]==d["State"]);
-					//d_sight_curr = d_sight.filter(d_s => d_s["Start_Year"] == start_year && d_s["Stop_Year"] == end_year && d_s["State"]==d["State"]);
-					//d_sight_curr = [d_sight[get_state_year_idx(d["State"], start_year, end_year)]]//changed for full rel
 					if (xvar == 'Num_Sightings') {
-						//changed for x axis
 						d_sight_curr = [d_sight_abs[get_state_year_idx(d['State'], start_year, end_year)]];
 					} else {
 						d_sight_curr = [d_sight[get_state_year_idx(d['State'], start_year, end_year)]];
 					}
-
-					//d["Num_Sightings"] = d_sight_curr[0]["Num_Sightings"];
-					//d["Rel_Num_Sightings"] = d_sight_curr[0]["Rel_Num_Sightings"]; //changed for full rel
-					d['Rel_Num_Sightings'] = d_sight_curr[0][xvar]; //changed for x axis
+					d['Rel_Num_Sightings'] = d_sight_curr[0][xvar];
 					return;
 				});
 
 				// create scales based on selections
 				// the domain will be modified when selections change
 				if (xvar == 'Num_Sightings') {
-					//changed for x axis
 					x_domain_max = d3.max([1, d3.max(d_other.map((d) => d['Rel_Num_Sightings']))]);
 				} else {
 					x_domain_max = d3.max(d_other.map((d) => d['Rel_Num_Sightings']));
@@ -386,23 +366,21 @@
 
 				xScale = d3
 					.scaleLinear()
-					//.domain([0, d3.max([1, d3.max(d_other.map(d => d[xvar]))])])//changed for full rel
-					//.domain([0, 1, d3.max(d_other.map(d => d[xvar]))]) //changed for x axis, evtl Fehler drin und man müsste 1 sowieso rausnehmen
-					.domain([0, x_domain_max]) //changed for x axis
+					.domain([0, x_domain_max])
 					.range([0, innerWidth]);
-				xAxis = d3.axisBottom(xScale).tickSize(-innerHeight);
+				xAxis = d3.axisBottom(xScale).tickSize(-innerHeight).tickFormat(axis_label_format);
 
 				yScale = d3
 					.scaleLinear()
 					.domain([0, d3.max(d_other.map((d) => d[yvar]))])
 					.range([0, innerHeight].reverse());
-				yAxis = d3.axisLeft(yScale).tickSize(-innerWidth);
+				yAxis = d3.axisLeft(yScale).tickSize(-innerWidth).tickFormat(axis_label_format);
 
 				// create axes
 				scatterInner
 					.append('g')
 					.attr('transform', 'translate(' + 0 + ', ' + innerHeight + ')')
-					.attr('class', 'x axis') // note: two classes; handy!
+					.attr('class', 'x axis')
 					.call(xAxis);
 
 				scatterInner.append('g').attr('class', 'y axis').call(yAxis);
@@ -423,47 +401,27 @@
 					.attr('text-anchor', 'middle')
 					.attr('transform', `rotate(-90 ${margins.left / 2} ${margins.top + innerHeight / 2})`)
 					.text(longVars[yvar]);
-
-				//var slider = createD3RangeSlider(0, 10, "#slider-container");
-				/*
-			rangeSlider(document.querySelector('#slider-container'), {
-			//orientation: 'vertical'
-			// min value
-			min: 1910,
-			// max value
-			max: 2023,
-			
-			// step size
-			step: 1,
-			});
-
-			d3.select("div#slider3")
-						.call(d3.slider().axis(true).min(0).max(10).value([2,4])
-						.on("slide", function(evt,value) {
-							dispatch.filter(value);
-							d3.select("#slider3textmin").text(Math.floor(value[0]));
-							d3.select("#slider3textmax").text(Math.floor(value[1]))
-						}))*/
 			}
 
 			// update elements that get modified when selections change
-			//function update(d_other, d_sight) {
 			function update(d_other, d_sight, d_sight_abs) {
-				//let xvar = 'Num_Sightings'
-				//let xvar = 'Rel_Num_Sightings' //changed for full res
-				let xvar = d3.select('select.xvar').property('value'); //changed for x axis
+				let xvar = d3.select('select.xvar').property('value');
 				let yvar = d3.select('select.yvar').property('value');
 				let start_year = +d3.select('#button_start_year').property('value');
 				let end_year = +d3.select('#button_end_year').property('value');
 
-				if (start_year<min_year) { //changed for botton
+				if (start_year<min_year) {
 					d3.select("#button_start_year").property('value', min_year)
 				}
 				if (end_year>max_year) {
 					d3.select("#button_end_year").property('value', max_year)
 				}
-				if (start_year>end_year) {
+				if (start_year>end_year && end_year>=min_year) {
 					d3.select("#button_start_year").property('value', end_year)
+				}
+				if (start_year>end_year && end_year<min_year) {
+					d3.select("#button_start_year").property('value', min_year)
+					d3.select("#button_end_year").property('value', min_year)
 				}
 				start_year = +d3.select("#button_start_year").property('value')
 				end_year = +d3.select("#button_end_year").property('value')
@@ -479,37 +437,26 @@
 				);
 				d3.select('#button_end_year').property('max', max_year);
 
-				//change Num_sightings in d_other s.t. they refelct the chosen year range
+				//change Num_sightings in d_other s.t. they reflect the chosen year range
 				d_other.map((d) => {
-					//d_sight_curr = d_sight.filter(d_s => d_s["Year"] == start_year && d_s["State"]==d["State"]);
-					//d_sight_curr = d_sight.filter(d_s => d_s["Start_Year"] == start_year && d_s["Stop_Year"] == end_year && d_s["State"]==d["State"]);
-					//d_sight_curr = [d_sight[get_state_year_idx(d["State"], start_year, end_year)]]//changed for full rel
-
 					if (xvar == 'Num_Sightings') {
-						//changed for x axis
 						d_sight_curr = [d_sight_abs[get_state_year_idx(d['State'], start_year, end_year)]];
 					} else {
 						d_sight_curr = [d_sight[get_state_year_idx(d['State'], start_year, end_year)]];
 					}
-
-					//d["Num_Sightings"] = d_sight_curr[0]["Num_Sightings"];
-					//d["Rel_Num_Sightings"] = d_sight_curr[0]["Rel_Num_Sightings"];//changed for full res
-					d['Rel_Num_Sightings'] = d_sight_curr[0][xvar]; //changed for x axis
+					d['Rel_Num_Sightings'] = d_sight_curr[0][xvar];
 					return;
 				});
 
 				// update scales
 				// the domain will be modified when selections change
 				if (xvar == 'Num_Sightings') {
-					//changed for x axis
 					x_domain_max = d3.max([1, d3.max(d_other.map((d) => d['Rel_Num_Sightings']))]);
 				} else {
 					x_domain_max = d3.max(d_other.map((d) => d['Rel_Num_Sightings']));
 				}
 
-				//xScale.domain([0, d3.max([1, d3.max(d_other.map(d => d[xvar]))])])//changed for full rel
-				//xScale.domain([0, d3.max(d_other.map(d => d[xvar]))])
-				xScale.domain([0, x_domain_max]); //changed for x axis
+				xScale.domain([0, x_domain_max]);
 				yScale.domain([0, d3.max(d_other.map((d) => d[yvar]))]);
 
 				// update axes
@@ -524,12 +471,12 @@
 					.append('circle')
 					.attr('cx', function (d) {
 						return xScale(d['Rel_Num_Sightings']);
-					}) // changed for x axis
+					})
 					.attr('cy', function (d) {
 						return yScale(d[yvar]);
 					})
 					.attr('r', 4)
-					.append('title') // TITLE APPENDED HERE
+					.append('title') // display state name when hovering over point
 					.text(function (d) {
 						return longStates[d['State']];
 					});
@@ -543,13 +490,13 @@
 					.duration(transitionTime)
 					.attr('cx', function (d) {
 						return xScale(d['Rel_Num_Sightings']);
-					}) // changed for x axis
+					})
 					.attr('cy', function (d) {
 						return yScale(d[yvar]);
 					});
 
 				// axis labels
-				scatterOuter //changed for x axis
+				scatterOuter
 					.selectAll('text.x.axis') // select text elements with two both classes
 					.transition()
 					.duration(transitionTime)
